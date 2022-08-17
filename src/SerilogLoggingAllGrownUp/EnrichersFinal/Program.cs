0c436c8c5c3123f30932034c37364937a34cb250
Brian Korzynski <korzynski@live.com>
2022-08-17 16:23:41 -0400

    cleanup

--- src/SerilogLoggingAllGrownUp/EnrichersFinal/Program.cs
+++ src/SerilogLoggingAllGrownUp/EnrichersFinal/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.IO;
 using Serilog;
 
-namespace EnrichersFinal
+namespace Enrichers
 {
     class Program
     {
+        static Program()
+        {
+            Directory.GetFiles(@"C:\temp\Logs").Foreach(File.Delete);
+        }
+
         static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
@@ -12,7 +18,13 @@ namespace EnrichersFinal
                 .Enrich.WithMachineName()
                 .Enrich.WithProperty("Version", "1.0.0")
                 .CreateLogger();
-            
+
+            GlobalEnrichment();
+            ContextualEnchriment();
+        }
+
+        static void GlobalEnrichment()
+        {
             Log.Logger.Information("Starting Service");
 
             try
@@ -26,6 +38,28 @@ namespace EnrichersFinal
             }
 
             Log.Logger.Information("Stopping Service");
+
+            Console.ReadKey();
+        }
+
+        static void ContextualEnchriment()
+        {
+            var log = new LoggerConfiguration().Enrich.FromLogContext();
+            log.Enrich.WithProperty("CustomerId", 12345);
+
+            log.Information("Starting Service");
+
+            try
+            {
+                log.Warning("Trying to do something");
+                throw new InvalidOperationException("You can't do that.");
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex, "I don't know something happed.");
+            }
+
+            log.Information("Stopping Service");
 
             Console.ReadKey();
         }
