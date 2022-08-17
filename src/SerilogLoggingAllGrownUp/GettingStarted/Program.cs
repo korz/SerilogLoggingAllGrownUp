bf5876825d8eaa7a2336b9e29e90f7955b5a9940
Brian Korzynski <korzynski@live.com>
2022-08-17 15:43:18 -0400

    cleanup

--- src/SerilogLoggingAllGrownUp/GettingStarted/Program.cs
+++ src/SerilogLoggingAllGrownUp/GettingStarted/Program.cs
@@ -11,23 +11,28 @@ namespace GettingStarted
             FirstLook();
             StaticInstance();
             CertainLevel();
+            MultipleLoggers();
         }
 
         static void FirstLook()
         {
+            //Create the logger
             ILogger logger = new LoggerConfiguration()
                 .WriteTo.RollingFile(@"C:\temp\logs\log.txt", LogEventLevel.Information)
                 .CreateLogger();
 
+            //Log something
             logger.Information("Did something");
             //Log.Logger.Information();
         }
 
         static void StaticInstance()
         {
+            //Create the logger
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.File(@"C:\Temp\logs\log.txt").CreateLogger();
 
+            //Log something
             Log.Logger.Information("Starting Service");
 
             try
@@ -45,10 +50,12 @@ namespace GettingStarted
 
         static void CertainLevel()
         {
+            //Create the logger
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.File(@"C:\Temp\logs\log.txt"
                 , LogEventLevel.Warning).CreateLogger();
 
+            //Log something
             Log.Logger.Information("Starting Service");
 
             try
@@ -62,6 +69,23 @@ namespace GettingStarted
             }
 
             Log.Logger.Information("Stopping Service");
+        }
+
+        static void MultipleLoggers()
+        {
+            //Create the logger
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.RollingFile(pathFormat: @"C:\temp\logs\log.txt")
+                .CreateLogger();
+
+            ILogger logger2 = new LoggerConfiguration()
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+
+            //Log something
+            logger.Information("Did something");
+            logger2.Information("Log something to the console");
+            //logger.Information();
         }
     }
 }
